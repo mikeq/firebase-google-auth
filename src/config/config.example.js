dd94b2c94439af97d6e893757aed73382957e858
Mike Quinn <mike@velochallenge.org>
2017-10-17 07:42:55 +0100

    Add Firebase dependencies

---
+++ src/config/config.example.js
@@ -0,0 +1,4 @@
+export default {
+  APIKey: '', // found on Firebase project settings|general
+  authorisedDomain: 'project-blahblah.firebaseapp.com' // found on Firebase project Authentication|Sign-in Method
+};
